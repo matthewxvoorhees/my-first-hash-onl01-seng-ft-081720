0215f466405d202481ba345e9987d29125a7868e
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-19 23:54:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,13 @@
 def my_hash
   murderers = {"Halloween" => "Michael Myers", "Friday the 13th" => "Jason Voorhees"}
-
-  
 end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings \
+  the_manifest = 
 
 
 end
